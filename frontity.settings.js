1dacd67c894c18ca2249233e5ac0f708e2a29e40
Mr.Robi <64766837+Md-Robiul-Islam-007@users.noreply.github.com>
2020-11-09 12:11:57 +0600

    Rely Trade Theme Installed

--- frontity.settings.js
+++ frontity.settings.js
@@ -9,37 +9,37 @@ const settings = {
   },
   "packages": [
     {
-      "name": "@frontity/twentytwenty-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
-          ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+      "name": "Rely-Trade",
+      // "state": {
+      //   "theme": {
+      //     "menu": [
+      //       [
+      //         "Home",
+      //         "/"
+      //       ],
+      //       [
+      //         "Nature",
+      //         "/category/nature/"
+      //       ],
+      //       [
+      //         "Travel",
+      //         "/category/travel/"
+      //       ],
+      //       [
+      //         "Japan",
+      //         "/tag/japan/"
+      //       ],
+      //       [
+      //         "About Us",
+      //         "/about-us/"
+      //       ]
+      //     ],
+      //     "featured": {
+      //       "showOnList": false,
+      //       "showOnPost": false
+      //     }
+      //   }
+      // }
     },
     {
       "name": "@frontity/wp-source",
---
+++ packages/Rely-Trade/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <>
+      <h1> Welcome To Rely Trade Theme </h1>
+      You can edit your package in:
+      <pre>packages\Rely-Trade\src\index.js</pre>
+    </>
+  );
+};
+
+export default {
+  name: "Rely-Trade",
+  roots: {
+    theme: Root
+  },
+  state: {
+    theme: {}
+  },
+  actions: {
+    theme: {}
+  }
+};
